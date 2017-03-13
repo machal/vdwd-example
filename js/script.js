791a9aa50aadb29535c0b0cb626aa43bbd880a5b
Martin Michálek <michalek@shortcat.cz>
2017-03-13 11:29:06 +0100

    Komponenta Stepper: Pridani do kosiku pomoci tlacitek

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 jQuery(document).ready(function($) {
 
+  // Tabulka s variantami
+
   $('.variants-table').on('click', 'td', function(event) {
     $(this).closest('tr').find('.variants-table-radio input[type=radio]').attr('checked', 'checked');
+  });
+
+  // Stepper, pridavani nebo odebirani hodnoty v inputu
+
+  $('.stepper .stepper-plus').on('click', function(){
+    $(this).siblings('.stepper-input').val(parseInt($(this).siblings('.stepper-input').val()) + 1);
+    return false;
+  });
+
+  $('.stepper .stepper-minus').on('click', function(){
+    $(this).siblings('.stepper-input').val(parseInt($(this).siblings('.stepper-input').val()) - 1);
+    return false;
   });
 
 });
